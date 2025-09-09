7649c125504ab0e6c487f5c8cf459c6e972e3218
Albert Lee <koresaremi2001@gmail.com>
2025-09-09 18:57:12 +0600

    urok 3

--- lesson3.py
+++ lesson3.py
@@ -1,30 +1,30 @@
-class Car:
-
-    def __init__(self, color, model): #inicializator
-        self.color = color
-        self.model = model
-        self._fined = False #защищенный атрибут
-        self.__testt = False #приватный атрибут (можно юзать только внутри класса,на классы приемники не распростроняется)
-
-    def drive_to_location(self,location ):
-        print(f'Car {self.model} is driving to {location}')
-
-    def _test_car(self):
-        print('test') #защищенный метод
-
-    def get_test(self):
-        return self.__testt #getter
-
-    def set_test(self, testt):
-        self.__testt = testt #setter
-
-class Bus(Car):
-    def test_bus(self):
-        print(f'test bus {self.get_test()}')
-
-bus_40 = Bus('green', '400')
-
-bus_40.test_bus()
+# class Car:
+#
+#     def __init__(self, color, model): #inicializator
+#         self.color = color
+#         self.model = model
+#         self._fined = False #защищенный атрибут
+#         self.__testt = False #приватный атрибут (можно юзать только внутри класса,на классы приемники не распростроняется)
+#
+#     def drive_to_location(self,location ):
+#         print(f'Car {self.model} is driving to {location}')
+#
+#     def _test_car(self):
+#         print('test') #защищенный метод
+#
+#     def get_test(self):
+#         return self.__testt #getter
+#
+#     def set_test(self, testt):
+#         self.__testt = testt #setter
+#
+# class Bus(Car):
+#     def test_bus(self):
+#         print(f'test bus {self.get_test()}')
+#
+# bus_40 = Bus('green', '400')
+#
+# bus_40.test_bus()
 
 # @property (getter toje)
 
@@ -41,3 +41,18 @@ bus_40.test_bus()
 # car_honda.color = 'green'
 
 # print(car_honda.color)
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):      #абстракция (используется как шаблон для других классов,в нём ничего не реализуется)
+    @abstractmethod
+    def make_sound(self)
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        print('sobaka laet')
+
+dog = Dog()
+
+dog.make_sound()
