c189f5a231d7d53410618d24c3f16878dfdc8735
Lee Albert <koresaremi2001@gmail.com>
2025-09-18 17:41:52 +0600

    вернул объект в магических методах,вместо f строки

--- homeworks/distance.py
+++ homeworks/distance.py
@@ -2,18 +2,19 @@ class Distance:
     def __init__(self, value , unit):
         self.value = value
         self.unit = unit
+        self.average_unit = 'm'
 
     def __str__(self):
-        return f"Дистанция : {self.convert()} m"
+        return F'Дистанция = {self.convert()}{self.average_unit}'
 
     def __add__(self, other):
-        return f"{self.convert() + other.convert()} m"
+        return Distance((self.convert() + other.convert()), self.average_unit)
 
     def __sub__(self, other):
         result = self.convert() - other.convert()
         if result < 0:
             return 'ne mogu vi4est iz menshego bolshee(('
-        return f"{result} m"
+        return Distance(result, self.average_unit)
 
 
     def __eq__(self, other):
@@ -26,6 +27,3 @@ class Distance:
             return self.value * 1000
         elif self.unit == "cm":
             return self.value * 0.01
-
-
-
