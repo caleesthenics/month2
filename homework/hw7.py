deb471b7de44d64801dd5ac527fb931927e4b73b
Lee Albert <koresaremi2001@gmail.com>
2025-09-25 21:50:15 +0600

    peredelald dz

--- homework/hw7.py
+++ homework/hw7.py
@@ -28,8 +28,8 @@ def add_book(name, author, publication_year, genre, number_of_pages, number_of_c
                  (name, author, publication_year, genre, number_of_pages, number_of_copies))
     conn.commit()
 
-def soft_delete_book(zna4enie, book_id):
-    conn.execute("UPDATE books SET deleted = ? WHERE id = ?", (zna4enie, book_id))
+def soft_delete_book(book_id):
+    conn.execute("UPDATE books SET deleted = 1 WHERE id = ?", (book_id,))
     cursor = conn.execute("SELECT name, author, publication_year, genre, number_of_pages, number_of_copies FROM books WHERE id = ?", (book_id,))
     row = cursor.fetchone()
     conn_archive.execute("""INSERT INTO archive_books (name, author, publication_year, genre, number_of_pages, number_of_copies) VALUES (?, ?, ?, ?, ?, ?)""", row)
@@ -50,10 +50,10 @@ if __name__ == '__main__':
 
     create_table()
 
-    counter = 0
-    while counter < 10:
-        counter += 1
-        add_book('hz', 'kto-to', '1818', 'detective', '8','10')
+    # counter = 0
+    # while counter < 10:
+    #     counter += 1
+    #     add_book('hz', 'kto-to', '1818', 'detective', '8','10')
 
-    # soft_delete_book('1','3')
+    # soft_delete_book(3)
     hard_delete_book(3)
